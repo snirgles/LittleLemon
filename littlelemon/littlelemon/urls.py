2e6a3cd503117fd2090011766de2d4a1d156450f
Lindsey <lfwachtel@gmail.com>
2024-01-12 19:07:06 -0500

    Include app URL config

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import sayHello
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', sayHello, name='sayHello'),
 ]
