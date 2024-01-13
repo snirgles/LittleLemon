acc4f1d5087180d5e7270dd8f4b5e962fbc78ec9
Lindsey <lfwachtel@gmail.com>
2024-01-12 19:45:26 -0500

    moving restaurant

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,24 +1,8 @@
-"""littlelemon URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#update URLConf by including URL patterns of restaurant app
 from django.contrib import admin
-from django.urls import path
-from . import views
-
-from .views import sayHello
+from django.urls import path, include
 
 urlpatterns = [
-    path('', sayHello, name='sayHello'), path(''. views.index, name='index')
+   path('admin/', admin.site.urls),
+   path(restaurant/', include(restaurant.urls'))
 ]
