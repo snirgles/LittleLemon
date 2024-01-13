392db6dd3646066b7892853b95d1df8ec7c685e6
Lindsey <lfwachtel@gmail.com>
2024-01-12 19:28:31 -0500

    see if this puts views where it should be

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 from .views import sayHello
 
 urlpatterns = [
-    path('', sayHello, name='sayHello'),
+    path('', sayHello, name='sayHello'), path(''. views.index, name='index')
 ]
