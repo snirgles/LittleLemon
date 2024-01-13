81887ac25398f9a8a837ead5fb2c2ad3bc194c34
Lindsey <lfwachtel@gmail.com>
2024-01-12 20:17:40 -0500

    actually install the restaurant app correctly (Restaurant)

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-from .views import sayHello
-
-urlpatterns = [
-    path('', sayHello, name='sayHello'), path(''. views.index, name='index')
+from django.contrib import admin 
+from django.urls import path 
+from .views import sayHello 
+  
+urlpatterns = [ 
+    path('', sayHello, name='sayHello'), 
 ]
--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,8 +1,8 @@
 #update URLConf by including URL patterns of restaurant app
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-   path('admin/', admin.site.urls),
-   path(restaurant/', include(restaurant.urls'))
+from django.contrib import admin 
+from django.urls import path, include  
+ 
+urlpatterns = [ 
+    path('admin/', admin.site.urls), 
+    path('restaurant/', include('restaurant.urls')),
 ]
